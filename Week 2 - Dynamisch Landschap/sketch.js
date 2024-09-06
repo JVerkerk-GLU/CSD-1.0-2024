688b1a20dc67130e3e65c4bee0e00fc434ef7b52
JirreV <jirreamon@gmail.com>
2024-09-06 14:16:31 +0200

    [/] Further development of Week 2 Exercise

--- Week 2 - Dynamisch Landschap/sketch.js
+++ Week 2 - Dynamisch Landschap/sketch.js
@@ -1,42 +1,178 @@
-const WIDTH_BUFFER = 128;
+const WINDOW_WIDTH = 800;
+const WINDOW_HEIGHT = 600;
+const WINDOW_MIN = -128;
+const WINDOW_MAX = WINDOW_WIDTH + 128;
 
-const CLOUD_HEIGHT = 64;
-const CLOUD_SIZE = 32;
-const CLOUD_SPEED = 1;
-const CLOUD_HIGHLIGHT_OFFSET = 8;
+let daySky, nightSky;
+const SKY_FADE_SPEED = 0.01;
+let isDay = true;
+let dayBrightness = 1.0;
+const STAR_COUNT = 64;
+let starPos = [];
 
 const SUN_HEIGHT = 64;
 const SUN_SIZE = 32;
 const SUN_SPEED = 0.5;
 const SUN_LIGHT_SPEED = 0.025;
 const SUN_LIGHT_OFFSET = 32;
+let sunX;
+
+const CLOUD_HEIGHT = 64;
+const CLOUD_SIZE = 32;
+const CLOUD_HIGHLIGHT_OFFSET = 8;
+const CLOUD_COUNT = 3;
+const CLOUD_MIN_SPEED = 0.5;
+const CLOUD_MAX_SPEED = 1.5;
+let dayCloud, nightCloud;
+let cloudSpeed = [];
+let cloudY = [96, 128, 160];
+let cloudX = [WINDOW_WIDTH * 0.2, WINDOW_WIDTH * 0.4, WINDOW_WIDTH * 0.75];
+
+let stopLight = 0;
 
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(WINDOW_WIDTH, WINDOW_HEIGHT);
   ellipseMode(RADIUS);
+
+  daySky = color(135, 206, 235);
+  nightSky = color(12, 20, 69);
+  dayCloud = 255;
+  nightCloud = 64;
+  for (let i = 0; i < STAR_COUNT; i++) {
+    starPos[i] = { x: random(0, WINDOW_WIDTH), y: random(0, WINDOW_HEIGHT) };
+  }
+  sunX = random(WINDOW_MIN, WINDOW_MAX);
+  for (let i = 0; i < CLOUD_COUNT; i++) {
+    cloudX[i] = random(WINDOW_MIN, WINDOW_MAX);
+    cloudSpeed[i] = random(CLOUD_MIN_SPEED, CLOUD_MAX_SPEED);
+  }
 }
 
 function draw() {
-  background(135, 206, 235);
+  if (isDay) {
+    dayBrightness = min(dayBrightness + SKY_FADE_SPEED, 1.0);
+  } else {
+    dayBrightness = max(dayBrightness - SKY_FADE_SPEED, 0.0);
+  }
 
-  let sunX = ((frameCount * SUN_SPEED) % (width + WIDTH_BUFFER * 2)) - WIDTH_BUFFER;
-  noStroke();
-  fill(255, 255, 0, 64);
-  circle(sunX, SUN_HEIGHT, SUN_SIZE + cos(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
-  circle(sunX, SUN_HEIGHT, SUN_SIZE + sin(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
-  circle(sunX, SUN_HEIGHT, SUN_SIZE - cos(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
-  circle(sunX, SUN_HEIGHT, SUN_SIZE - sin(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
-  fill(255,255,0);
-  circle(sunX, SUN_HEIGHT, SUN_SIZE);
+  //#region Sky
+  background(lerpColor(nightSky, daySky, dayBrightness));
+  stroke(255, 255, 0, 255 - (dayBrightness * 255));
+  strokeWeight(2);
+  for (let i = 0; i < STAR_COUNT; i++) {
+    point(starPos[i].x, starPos[i].y);
+  }
 
-  let cloudX = (width + WIDTH_BUFFER) - (frameCount * CLOUD_SPEED) % (width + WIDTH_BUFFER * 2);
-  fill(210)
+  //#region Sun
+  sunX += SUN_SPEED;
+  if (sunX > WINDOW_MAX) {
+    sunX = WINDOW_MIN;
+    isDay = !isDay;
+  }
   noStroke();
-  circle(cloudX, CLOUD_HEIGHT, CLOUD_SIZE);
-  circle(cloudX - CLOUD_SIZE * 1.25, CLOUD_HEIGHT, CLOUD_SIZE * 0.75);
-  circle(cloudX + CLOUD_SIZE * 1.25, CLOUD_HEIGHT + 8, CLOUD_SIZE * 0.75);
-  fill(255)
-  circle(cloudX, CLOUD_HEIGHT + CLOUD_HIGHLIGHT_OFFSET, CLOUD_SIZE);
-  circle(cloudX - CLOUD_SIZE * 1.25, CLOUD_HEIGHT + CLOUD_HIGHLIGHT_OFFSET, CLOUD_SIZE * 0.75);
-  circle(cloudX + CLOUD_SIZE * 1.25, CLOUD_HEIGHT + CLOUD_HIGHLIGHT_OFFSET + 8, CLOUD_SIZE * 0.75);
+  if (isDay)
+  {
+    fill(255, 128, 0, 64);
+    circle(sunX, SUN_HEIGHT, SUN_SIZE + cos(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
+    circle(sunX, SUN_HEIGHT, SUN_SIZE + sin(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
+    circle(sunX, SUN_HEIGHT, SUN_SIZE - cos(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
+    circle(sunX, SUN_HEIGHT, SUN_SIZE - sin(frameCount * SUN_LIGHT_SPEED) * SUN_LIGHT_OFFSET);
+    fill(255,255,0);
+    circle(sunX, SUN_HEIGHT, SUN_SIZE);
+  } else {
+    fill(220);
+    circle(sunX, SUN_HEIGHT, SUN_SIZE);
+    fill(nightSky);
+    circle(sunX + (SUN_SIZE * 0.5), SUN_HEIGHT, SUN_SIZE);
+  }
+
+
+  //#region Clouds
+  for (let i = 0; i < CLOUD_COUNT; i++) {
+    cloudX[i] -= cloudSpeed[i];
+    if (cloudX[i] < WINDOW_MIN) {
+      cloudX[i] = WINDOW_MAX;
+      cloudSpeed[i] = random(CLOUD_MIN_SPEED, CLOUD_MAX_SPEED);
+    }
+    fill(lerp(nightCloud, dayCloud, dayBrightness) - 32)
+    noStroke();
+    circle(cloudX[i], cloudY[i], CLOUD_SIZE);
+    circle(cloudX[i] - CLOUD_SIZE * 1.25, cloudY[i], CLOUD_SIZE * 0.75);
+    circle(cloudX[i] + CLOUD_SIZE * 1.25, cloudY[i] + 8, CLOUD_SIZE * 0.75);
+    fill(lerp(nightCloud, dayCloud, dayBrightness))
+    circle(cloudX[i], cloudY[i] + CLOUD_HIGHLIGHT_OFFSET, CLOUD_SIZE);
+    circle(cloudX[i] - CLOUD_SIZE * 1.25, cloudY[i] + CLOUD_HIGHLIGHT_OFFSET, CLOUD_SIZE * 0.75);
+    circle(cloudX[i] + CLOUD_SIZE * 1.25, cloudY[i] + CLOUD_HIGHLIGHT_OFFSET + 8, CLOUD_SIZE * 0.75);
+  }
+
+  //#region Mountains
+  stroke(0);
+  strokeWeight(1);
+  fill(32 + (32 * dayBrightness));
+  triangle(200, WINDOW_HEIGHT - 64, 600, WINDOW_HEIGHT - 64, 400, WINDOW_HEIGHT - 400);
+  fill(64 + (32 * dayBrightness));
+  triangle(128, WINDOW_HEIGHT - 64, 384, WINDOW_HEIGHT - 64, 256, WINDOW_HEIGHT - 256);
+  triangle(500, WINDOW_HEIGHT - 64, 900, WINDOW_HEIGHT - 64, 700, WINDOW_HEIGHT - 200);
+
+  //#region Ground
+  stroke(0, 32 + (32 * dayBrightness), 0);
+  strokeWeight(4);
+  fill(0, 64 + (32 * dayBrightness), 0);
+  rect(-32, WINDOW_HEIGHT - 64, WINDOW_WIDTH + 64, 128);
+
+  stroke(16 + (64 * dayBrightness));
+  fill(32 + (64 * dayBrightness));
+  rect(-32, WINDOW_HEIGHT -56, WINDOW_WIDTH + 64, 64);
+
+  stroke(128 + (64 * dayBrightness));
+  strokeWeight(8);
+  for (let x = -32; x < WINDOW_WIDTH + 32; x += 128) {
+    line(x, WINDOW_HEIGHT - 24, x + 64, WINDOW_HEIGHT - 24);
+  }
+
+  //#region Trees
+  noStroke();
+  fill(82, 52, 32);
+  // The indentation is intended to be increasing
+  for (let x = 32; x < WINDOW_WIDTH; x += max(x, 80)) {
+    fill(41 + (41 * dayBrightness), 26 + (26 * dayBrightness), 16 + (16 * dayBrightness));
+    rect(x, WINDOW_HEIGHT - 200, 16, 140);
+    fill(0, 48 + (48 * dayBrightness), 0);
+    circle(x + 8 + cos(frameCount * 0.1) * 4, WINDOW_HEIGHT - 200, 32);
+    fill(0, 32 + (48 * dayBrightness), 0);
+    circle(x + 4 + sin(frameCount * 0.1) * 4, WINDOW_HEIGHT - 196, 32);
+    fill(0, 16 + (48 * dayBrightness), 0);
+    circle(x - sin(frameCount * 0.1) * 4, WINDOW_HEIGHT - 192, 32);
+    fill(0, 8 + (48 * dayBrightness), 0);
+    circle(x - 4 - cos(frameCount * 0.1) * 4, WINDOW_HEIGHT - 196, 32);
+
+    //#region Traffic Light
+    stopLight = floor(frameCount / 300) % 3;
+
+    stroke(48);
+    const tlX = WINDOW_WIDTH - 128;
+    strokeWeight(8);
+    line(tlX, WINDOW_HEIGHT - 56, tlX, WINDOW_HEIGHT - 128);
+    noStroke();
+    fill(48);
+    rect(tlX - 16, WINDOW_HEIGHT - 222, 32, 96);
+    if (stopLight == 0) {
+      fill(255, 0, 0);
+    } else {
+      fill(64, 0, 0);
+    }
+    circle(tlX, WINDOW_HEIGHT - 206, 12);
+    if (stopLight == 2) {
+      fill(255, 64, 0);
+    } else {
+      fill(64, 32, 0);
+    }
+    circle(tlX, WINDOW_HEIGHT - 176, 12);
+    if (stopLight == 1) {
+      fill(0, 255, 0);
+    } else {
+      fill(0, 64, 0);
+    }
+    circle(tlX, WINDOW_HEIGHT - 144, 12);
+  }
 }
