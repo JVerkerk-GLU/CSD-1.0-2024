a9d27db8a6940be6dd7e1d17a105054688fd607a
JirreV <jirreamon@gmail.com>
2024-09-25 11:34:44 +0200

    [+] Finished Week 4 exercise [/] Updated Readme

--- Week 4 - Generatieve Kunst/sketch.js
+++ Week 4 - Generatieve Kunst/sketch.js
@@ -18,36 +18,33 @@ function setup() {
   angleMode(DEGREES);
   colorMode(HSB, 360, 255, 255);
   noStroke();
+  // Create a fun starting amount of shapes
   amount = round(random(0.25, 0.75) * SPAWN_AMOUNT);
-  for (let i = 0; i < amount; i++)
-  {
-    createShape();
+  for (let i = 0; i < amount; i++) {
+    shapes.push(new Shape());
   }
 }
 
 function draw() {
   background(128);
+  // every frame there is a chance to spawn a new shape, this spawn chance is based on the current amount of shapes
   if (random(1) < (BASE_SPAWN_CHANCE - SPAWN_CHANCE_PENALTY * shapes.length)) {
-    createShape();
+    shapes.push(new Shape());
   }
 
+  // Without any shapes to render, there is no need to go through the for loop
   if (shapes.length <= 0) {
     return;
   }
 
-  for (let i = 0; i < shapes.length; i++)
-  {
+  for (let i = 0; i < shapes.length; i++) {
     if (shapes[i].isAlive()) {
       shapes[i].draw();
-    } else {
+    } else { // Remove a shape if it is no longer alive
       shapes.splice(i, 1);
       i--;
     }
   }
-}
-
-function createShape() {
-  shapes.push(new Shape());
 }
 
 function Shape() {
@@ -67,12 +64,14 @@ function Shape() {
   this.speed = random(MAX_SPEED);
 
   this.isAlive = function () {
+    // Is the current time less than its maximum lifetime
     return millis() * 0.001 < this.startTime + this.lifetime;
   }
 
   this.getShape = function (padding) {
     beginShape(TRIANGLE_FAN);
     vertex(0, 0);
+    // Every shape is a circle, just with different amount of vertexes
     let delta = 360.0 / this.sides;
     for(let i = 0; i <= this.sides; i++)
     {
@@ -83,17 +82,22 @@ function Shape() {
 
   this.draw = function () {
     push();
+    // Slowly move the shape based on the direction and speed
     this.x += cos(this.direction) * this.speed * deltaTime * 0.001;
     this.y += sin(this.direction) * this.speed * deltaTime * 0.001;
     translate(this.x, this.y);
     rotate(this.rotation + (millis() * 0.001 * this.rotSpeed));
+    //Base the size on its age
     let alpha = sin(
       (((millis() * 0.001) - this.startTime) / this.lifetime) * 180.0
     );
     
+    // Scale the shape based on its alpha
     scale(alpha);
+    // Draw the outline
     fill(0, 0, 0, alpha);
     this.getShape(12);
+    // Draw the inner shape
     this.color.setAlpha(alpha);
     fill(this.color);
     this.getShape(0);
